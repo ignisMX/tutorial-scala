15ddc06c9b4907629f807ea80b637fbf93cf7197
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2025-05-22 04:21:47 +0000

    it adds function examples

---
+++ src/main/scala/Functions.scala
@@ -0,0 +1,24 @@
+object Functions {
+
+  def main(args: Array[String]): Unit = {
+    println(isEven(3))
+    println(isEven(4))
+
+    println(namedArguments(1, "Republic of devs"))
+    println(namedArguments(text = "Republic of devs", number = 1))
+
+    println(defaultValue(1, "Keep coding"))
+    println(defaultValue(text = "Keep coding", number = 2))
+    println(defaultValue(number = 3))
+    println(defaultValue(3))
+  }
+
+  private def isEven(number: Int): Boolean = {
+    number % 2 == 0
+  }
+
+  private def namedArguments(number: Int, text: String): String = s"$number - $text"
+
+  private def defaultValue(number: Int, text: String = "Republic of devs"): String = s"$number - $text"
+
+}
